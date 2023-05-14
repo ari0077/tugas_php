59d18a2cf7adcdbbb2f61e2529feb84500553d35
ari0077 <22020110002@std.umkudus.ac.id>
2023-05-14 21:52:24 +0700

    tgs php 14

---
+++ pertemuan 14/admin/membercontroller.php
@@ -0,0 +1,29 @@
+<?php 
+
+session_start();
+include_once 'koneksi.php';
+include_once 'models/Member.php';
+
+$username = $_POST ['username'];
+$password = $_POST ['password'];
+
+$data = [
+    $username,
+    $password
+];
+
+$model = new Member();
+$rs = $model->cekLogin($data);
+if(isset($rs)){
+    $_SESSION['MEMBER'] = $rs;
+    header('Location:http://localhost/belajarPHP/webnative/admin/index.php?url=product');
+}
+else {
+    echo '<script> alert("user/password anda salah");history.back();</script>';
+
+}
+
+
+
+
+?>
